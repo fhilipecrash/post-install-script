ec7627494b5f3e14a15141ae772c9142e3e0718d
Fhilipe Coelho <coelhocrash11@gmail.com>
2022-03-14 21:31:56 -0300

    improved arch installation with function

--- post-install.sh
+++ post-install.sh
@@ -17,25 +17,26 @@
 # HOW TO USE?
 #   $ ./post-install.sh
 #
-# Changelog:
-#
-#
-#   v0.1 2022-03-09, Fhilipe Coelho:
-#      - First version with some tweaks to Arch Linux install
-#   v0.2 2022-03-13, Fhilipe Coelho:
-#      - Arch Linux installation finished and mac installation started
-#
 # License: GPL v3.
 #
 
-# TODO add screens to script with dialog
-#
-# dialog --clear --backtitle "Post-install script" --title "Welcome to my script" --menu "Choose one of the following options:" 15 40 4 \
-# 1 "Option 1" \
-# 2 "Option 2" \
-# 3 "Option 3"
-#
-#
+# Variables
+pkg_list=""
+
+# Function
+add_pkg() {
+  while true; do
+    read -p "$2 [Y/n]" answer
+    answer=${answer:-y}
+    case $answer in
+      [Yy]* ) pkg_list+="$1 "; break;;
+      [Nn]* ) exit;;
+      * ) echo "Please answer Y or N";;
+    esac
+  done
+}
+
+clear
 echo $'Post install script by Fhilipe\nWould you like to perform the post install configuration? [Y/n]\n'
 
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
@@ -45,84 +46,117 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
     echo "Bye"
   else
     ## Enable multilib
-    echo "Enabling multilib repo"
-    sudo sed -i '/multilib\]/,+1 s/^#//' /etc/pacman.conf
+    echo "=====> Enabling multilib repo"
+    sudo sed -i '/multilib\]/,+1 s/^#//' /etc/pacman.conf > /dev/null
 
-    ## Git install
-    sudo pacman -Sy git
+    echo "=====> Installing Git"
+    yay -Sy git > /dev/null
 
     ## Clone Dotfiles
-    echo "Cloning Dotfiles"
+    echo "=====> Cloning Dotfiles"
     git clone https://github.com/FhilipeCrash/Dotfiles
 
     ## Yay install
-    echo "Installing Yay"
-    cd /tmp/
-    git clone https://aur.archlinux.org/yay.git
-    cd yay
-    makepkg -si
-    yay -S yay-git
+    echo "=====> Installing Yay"
+    {
+      git clone https://aur.archlinux.org/yay.git
+      cd yay
+      makepkg -si
+      yay -S yay-git
+    } > /dev/null
 
     ## Enable flatpak support
-    yay -S flatpak
+    echo "=====> Installing Flatpak"
+    yay -S flatpak > /dev/null
 
     ## Zsh install 
-    echo "Installing ZSH and Oh My Zsh"
-    yay -S zsh
-    sudo chsh -s /usr/bin/zsh
-    sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-    git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-    cp ~/Dotfiles/.zshrc ~/
-    cp ~/Dotfiles/.yarnrc ~/
-    cp -r ~/Dotfiles/.local/share/applications/ ~/.local/share/
+    echo "=====> Installing ZSH and Oh My Zsh"
+    {
+      yay -S zsh
+      sudo chsh -s /usr/bin/zsh
+      sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+      git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+      git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+      git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+      cp ~/Dotfiles/.zshrc ~/
+      cp ~/Dotfiles/.yarnrc ~/
+      cp -r ~/Dotfiles/.local/share/applications/ ~/.local/share/
+    } > /dev/null
 
     ## Xorg and drivers install
-    echo "Installing Xorg and drivers"
-    yay -S xorg-xinit xorg-xinit xorg-xkill xf86-video-intel 
+    echo "=====> Installing Xorg and drivers"
+    yay -S xorg-xinit xorg-xinit xorg-xkill xf86-video-intel > /dev/null
 
     ## Gnome minimal install
-    echo "Installing GNOME with mutter-rounded and Gtk theme"
-    yay -S gnome-shell gnome-tweak-tool gnome-control-center xdg-user-dirs \
+    echo "=====> Installing GNOME with mutter-rounded and Gtk theme"
+    {
+      yay -S gnome-shell gnome-tweak-tool gnome-control-center xdg-user-dirs \
       gdm mutter-rounded gnome-online-accounts nautilus-open-any-terminal \
       flat-remix-gnome flat-remix-gtk papirus-icon-theme papirus-folders-git
+    } > /dev/null
  
     ## Gnome tweaks
-    echo "Configuring GNOME"
-    gsettings set org.gnome.shell.app-switcher current-workspace-only true
-    gsettings set org.gnome.desktop.wm.preferences button-layout ‘close,minimize,maximize:appmenu’
-    gsettings set org.gnome.mutter round-corners-radius 12
-    gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacritty
-    gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK-Green-Dark"
-    gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
-    gsettings set org.gnome.shell.extensions.user-theme name "Flat-Remix-Green-Dark-fullPanel" 
-    papirus-folders -C teal --theme Papirus-Dark
+    echo "=====> Configuring GNOME"
+    {
+      gsettings set org.gnome.shell.app-switcher current-workspace-only true
+      gsettings set org.gnome.desktop.wm.preferences button-layout ‘,close,minimize,maximize:appmenu’
+      gsettings set org.gnome.mutter round-corners-radius 12
+      gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacritty
+      gsettings set org.gnome.desktop.interface gtk-theme "Flat-Remix-GTK-Green-Dark"
+      gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+      gsettings set org.gnome.shell.extensions.user-theme name "Flat-Remix-Green-Dark-fullPanel" 
+      papirus-folders -C teal --theme Papirus-Dark
+    } > /dev/null
 
     ## Install my programs
-    echo "Installing all environment programs"
-    echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
-    curl -O https://download.sublimetext.com/sublimehq-pub.gpg
-    sudo pacman-key --add sublimehq-pub.gpg
-    sudo pacman-key --lsign-key 8A8F901A
-    rm sublimehq-pub.gpg
-    yay -Sy alacritty nautilus file-roller unrar unzip p7zip zip gvfs-goa gvfs-google gvfs-mtp \
-      google-chrome-stable discord steam telegram-desktop qbittorrent vlc heroic-games-launcher-bin \
-      visual-studio-code-bin sublime-text \
-      wine-staging wine-mono wine-gecko lutris winetricks \
-      evince eog gparted gnome-disks libreoffice-fresh libreoffice-fresh-pt-br \
+    echo "=====> Installing all environment programs"
+    {
+      echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
+      curl -O https://download.sublimetext.com/sublimehq-pub.gpg
+      sudo pacman-key --add sublimehq-pub.gpg
+      sudo pacman-key --lsign-key 8A8F901A
+      rm sublimehq-pub.gpg
+    } > /dev/null
+
+    add_pkg google-chrome-stable "Install Google Chrome?"
+    add_pkg discord "Install Discord?"
+    add_pkg telegram-desktop "Install Telegram?"
+    add_pkg qbittorrent "Install qBittorrent?"
+    add_pkg vlc "Install VLC?"
+    add_pkg visual-studio-code-bin "Install VS Code?"
+    add_pkg sublime-text "Install Sublime Text?"
+    add_pkg "libreoffice-fresh libreoffice-fresh-pt-br" "Install LibreOffice?"
+    add_pkg heroic-games-launcher-bin "Install Heroic Games Launcher?"
+    add_pkg "steam steam-native-runtime" "Install Steam?"
+
+    while true; do
+      read -p "Install Spotify?[Y/n]" answer
+      answer=${answer:-y}
+      case $answer in
+        [Yy]* ) flatpak install flathub com.spotify.Client; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer Y or N";;
+      esac
+    done
+    
+    # FIXME This line of code is not working, fix it later
+    {
+      yay -S $pkg_list alacritty nautilus file-roller unrar unzip p7zip zip gvfs-goa gvfs-google gvfs-mtp \
+      wine-staging wine-mono wine-gecko lutris winetricks evince eog gparted gnome-disks \
       baobab gnome-calculator gnome-characters extension-manager qt5ct \
       pipewire pipewire-pulse lib32-pipewire lib32-pipewire-pulse wirepumbler pavucontrol \
       yarn nodejs npm exa
-    flatpak install flathub com.spotify.Client
+    } > /dev/null
 
     ## Lunarvim install
-    sudo pip install pynvim
-    sudo npm i -g neovim
-    bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+    {
+      sudo pip install pynvim
+      sudo npm i -g neovim
+      bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/master/utils/installer/install.sh)
+    } > /dev/null
     
     ## Enable GDM service
-    sudo systemctl enable gdm.service
+    sudo systemctl enable gdm.service > /dev/null
   fi
   
 elif [[ "$OSTYPE" == "darwin" ]]; then
@@ -150,7 +184,7 @@ elif [[ "$OSTYPE" == "darwin" ]]; then
     
     ## Install useful programs
     echo "Installing all environment programs"
-    brew install --cask discord spotify google-chrome telegram-desktop visual-studio-code sublime-text qbittorrent vlc \
-      node python
+    brew install --cask discord spotify google-chrome telegram-desktop visual-studio-code sublime-text qbittorrent vlc
+    brew node python
   fi
 fi
